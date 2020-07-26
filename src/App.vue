4f489f49d29dc68414af6681be796041b820179e
sunshine <weertz.joffrey@protonmail.ch>
2020-07-26 12:54:25 +0200

    Initialisation# Vue-function-api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ mesage }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { value } from 'vue-function-api';
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  setup() {
+    const message = value('Hello there ! 1337 CYBERPUNK-MAN');
+
+    return {
+      message,
+    }
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
